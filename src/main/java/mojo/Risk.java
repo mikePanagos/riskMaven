4b743ad9d4a0526b476f8fd08f69e903f3869f00
Oscar Tovar <oscar.alberto.tovar@gmail.com>
2018-09-22 09:24:24 -0500

    Added Risk.java class to hold main function.

---
+++ src/main/java/mojo/Risk.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package mojo;
+import java.util.*;
+import mojo.*;
+
+/**
+ * @author TeamMojo
+ *
+ */
+public class Risk {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		gui ui = new gui();
+		ui.start();
+	}
+
+}
