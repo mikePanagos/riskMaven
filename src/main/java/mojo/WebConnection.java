8d82cbcc3e62c64c2342cd6ab625f335baebcb7e
Michael Panagos <michael@technicool.com>
2018-10-19 00:02:52 -0500

    amazon s3 test case

--- src/main/java/mojo/WebConnection.java
+++ src/main/java/mojo/WebConnection.java
@@ -3,13 +3,26 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
 
+import com.amazonaws.AmazonClientException;
 import com.amazonaws.AmazonServiceException;
-import com.amazonaws.SdkClientException;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.Bucket;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.ListObjectsRequest;
+import com.amazonaws.services.s3.model.ObjectListing;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+
+
+import java.io.File;
 
 /**
  * @author oscartovar
@@ -27,53 +40,59 @@ public class WebConnection {
 		
 	};
 	
-	public void updateS3() {
-		final AmazonS3 s3 = AmazonS3ClientBuilder.defaultClient();
-		try {
-		    s3.putObject(bucket_name, key_name, new File(file_path));
-		} catch (AmazonServiceException e) {
-		    System.err.println(e.getErrorMessage());
-		    System.exit(1);
-		}
-		// AWSCredentials credentials = null;
-        // try {
-        //     credentials = new ProfileCredentialsProvider("default").getCredentials();
-        // } catch (Exception e) {
-        //     throw new AmazonClientException(
-        //             "Cannot load the credentials from the credential profiles file. " +
-        //             "Please make sure that your credentials file is at the correct " +
-        //             "location (C:\\Users\\Michael\\.aws\\credentials), and is in valid format.",
-        //             e);
-        // }
+	public String updateS3() {
+		// final AmazonS3 s3 = AmazonS3ClientBuilder.defaultClient();
+		// try {
+		//     s3.putObject(bucket_name, key_name, new File(file_path));
+		// } catch (AmazonServiceException e) {
+		//     System.err.println(e.getErrorMessage());
+		//     System.exit(1);
+		// }
+		AWSCredentials credentials = null;
+        try {
+            credentials = new ProfileCredentialsProvider("risky").getCredentials();
+        } catch (Exception e) {
+            throw new AmazonClientException(
+                    "Cannot load the credentials from the credential profiles file. " +
+                    "Please make sure that your credentials file is at the correct " +
+                    "location (C:\\Users\\Michael\\.aws\\credentials), and is in valid format.",
+                    e);
+        }
 
-        // AmazonS3 s3 = AmazonS3ClientBuilder.standard()
-        //     .withCredentials(new AWSStaticCredentialsProvider(credentials))
-        //     .withRegion("us-west-2")
-        //     .build();
-
-        // String bucketName = "my-first-s3-bucket-" + UUID.randomUUID();
-        // String key = "MyObjectKey";
-        // try {
+        AmazonS3 s3 = AmazonS3ClientBuilder.standard()
+            .withCredentials(new AWSStaticCredentialsProvider(credentials))
+            .withRegion("us-east-2")
+            .build();
+        String bucketName = "riskmichaelpanagos" ;
+        String key = "logger";
+        try {
           
-        //     System.out.println("Uploading a new object to S3 from a file\n");
-        //     s3.putObject(new PutObjectRequest(bucketName, key, createSampleFile()));
+            System.out.println("Uploading a new object to S3 from a file\n");
+            // File f= new File("/log.txt");
+
+            s3.putObject(new PutObjectRequest(bucketName, key, new File(file_path)));
+            return "successfully added log.txt to s3 bucket";
 
            
           
-        // } catch (AmazonServiceException ase) {
-        //     System.out.println("Caught an AmazonServiceException, which means your request made it "
-        //             + "to Amazon S3, but was rejected with an error response for some reason.");
-        //     System.out.println("Error Message:    " + ase.getMessage());
-        //     System.out.println("HTTP Status Code: " + ase.getStatusCode());
-        //     System.out.println("AWS Error Code:   " + ase.getErrorCode());
-        //     System.out.println("Error Type:       " + ase.getErrorType());
-        //     System.out.println("Request ID:       " + ase.getRequestId());
-        // } catch (AmazonClientException ace) {
-        //     System.out.println("Caught an AmazonClientException, which means the client encountered "
-        //             + "a serious internal problem while trying to communicate with S3, "
-        //             + "such as not being able to access the network.");
-        //     System.out.println("Error Message: " + ace.getMessage());
-        // }
+        } catch (AmazonServiceException ase) {
+            System.out.println("Caught an AmazonServiceException, which means your request made it "
+                    + "to Amazon S3, but was rejected with an error response for some reason.");
+            System.out.println("Error Message:    " + ase.getMessage());
+            System.out.println("HTTP Status Code: " + ase.getStatusCode());
+            System.out.println("AWS Error Code:   " + ase.getErrorCode());
+            System.out.println("Error Type:       " + ase.getErrorType());
+            System.out.println("Request ID:       " + ase.getRequestId());
+            return "failed added log.txt to s3 bucket";
+
+        } catch (AmazonClientException ace) {
+            System.out.println("Caught an AmazonClientException, which means the client encountered "
+                    + "a serious internal problem while trying to communicate with S3, "
+                    + "such as not being able to access the network.");
+            System.out.println("Error Message: " + ace.getMessage());
+            return "failed added log.txt to s3 bucket";
+
+        }
 	};
 	
 }
---
+++ src/test/java/mojo/AmazonS3Test.java
@@ -0,0 +1,21 @@
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import java.util.*;
+
+
+import mojo.*;
+//"successfully added log.txt to s3 bucket"
+
+public class AmazonS3Test{
+
+
+    @Test public void sendFileTest(){
+        WebConnection wc = new WebConnection();
+
+        System.out.println("\n\n\nTESTING S3\n\n\n");
+        assertTrue(" ",wc.updateS3().equals("successfully added log.txt to s3 bucket"));
+        System.out.println("\n\n\nTESTING S3 PASSED WITH FLYING COLORS\n\n\n");
+
+    }
+}
