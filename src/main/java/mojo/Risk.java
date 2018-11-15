af25a133d67c2a6b411cc326a8d4297846103ece
Oscar Tovar <oscar.alberto.tovar@gmail.com>
2018-11-15 11:57:14 -0600

    Updated RiskyBot to parse JSON Responses and grab information about itself using the getInfo method.

--- src/main/java/mojo/Risk.java
+++ src/main/java/mojo/Risk.java
@@ -4,6 +4,9 @@
 package mojo;
 import mojo.*;
 import mojo.bot.RiskyBot;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 /**
  * @author TeamMojo
@@ -17,8 +20,21 @@ public class Risk {
 	public static void main(String[] args) {
 		RiskyBot bot = new RiskyBot();
 		bot.getUpdates();
+		bot.getInfo();
 //		gui ui = new gui();
 //		ui.start();
+
+//		ApiContextInitializer.init();
+//
+//		// Instantiate Telegram Bots API
+//		TelegramBotsApi botsApi = new TelegramBotsApi();
+//
+//		// Register our bot
+//		try {
+//			botsApi.registerBot(new RiskyBot());
+//		} catch (TelegramApiException e) {
+//			e.printStackTrace();
+//		}
 	}
 
 }
--- src/main/java/mojo/bot/RiskyBot.java
+++ src/main/java/mojo/bot/RiskyBot.java
@@ -3,6 +3,11 @@ package mojo.bot;
 import java.net.*;
 import java.io.*;
 import org.json.*;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+//import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+//import org.telegram.telegrambots.meta.api.objects.Update;
+//import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
 import javax.net.*;
 import javax.net.ssl.HttpsURLConnection;
 
@@ -13,7 +18,6 @@ import javax.net.ssl.HttpsURLConnection;
 public class RiskyBot {
     int gameID = 0;
     String token = System.getenv("telegramToken");
-//    URL sendMessage = new URL("https://api.telegram.org/bot796705800:AAG4coiT3xqeHmbTfLbPRvMo5K6hEOzzipA/sendMessage?");
 
     public RiskyBot() {
         this.gameID = 1;
@@ -42,6 +46,86 @@ public class RiskyBot {
     }
 
     public void reply() {
-
+        try {
+            URL sendMessage = new URL("https://api.telegram.org/bot" + token + "/sendMessage?");
+            HttpsURLConnection apiRequest = (HttpsURLConnection)sendMessage.openConnection();
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    apiRequest.getInputStream()
+            ));
+            String currentLine = "";
+            String response = "";
+            while ((currentLine = in.readLine()) != null) {
+                // No need to print out anything.
+                response = response + currentLine;
+            }
+            System.out.println(response);
+            JSONObject jsonResponse = new JSONObject(response);
+            Object results = jsonResponse.get("result");
+            System.out.println(results);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
+    public void getInfo() {
+        try {
+            URL getInfo = new URL("https://api.telegram.org/bot" + token + "/getMe");
+            HttpsURLConnection apiRequest = (HttpsURLConnection)getInfo.openConnection();
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    apiRequest.getInputStream()
+            ));
+            String currentLine = "";
+            String response = "";
+            while ((currentLine = in.readLine()) != null) {
+                // No need to print out anything.
+                response = response + currentLine;
+            }
+            System.out.println(response);
+            JSONObject jsonResponse = new JSONObject(response);
+            Object results = jsonResponse.get("result");
+            System.out.println(results);
+            JSONObject test = jsonResponse.getJSONObject("result");
+            System.out.println("TEST");
+            System.out.println("Hello my name is " + test.get("username"));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    
+
+//    @Override
+//    public void onUpdateReceived(Update update) {
+//
+//        // We check if the update has a message and the message has text
+//        if (update.hasMessage() && update.getMessage().hasText()) {
+//            // Set variables
+//            String message_text = update.getMessage().getText();
+//            long chat_id = update.getMessage().getChatId();
+//
+//            SendMessage message = new SendMessage() // Create a message object object
+//                    .setChatId(chat_id)
+//                    .setText(message_text);
+//            try {
+//                execute(message); // Sending our message object to user
+//            } catch (TelegramApiException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
+//
+//    @Override
+//    public String getBotUsername() {
+//        // Return bot username
+//        // If bot username is @MyAmazingBot, it must return 'MyAmazingBot'
+//        return "MyAmazingBot";
+//    }
+//
+//    @Override
+//    public String getBotToken() {
+//        // Return bot token from BotFather
+//        return token;
+//    }
+
 }
