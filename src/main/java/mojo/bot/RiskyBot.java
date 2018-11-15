1a12954168583a45788f457623bf3c1e7e75f4ee
Michael Panagos <michael@technicool.com>
2018-11-15 14:41:18 -0600

    bot now asked for id of game

--- src/main/java/mojo/bot/RiskyBot.java
+++ src/main/java/mojo/bot/RiskyBot.java
@@ -1,6 +1,7 @@
 package mojo.bot;
 
 import java.net.*;
+import java.util.*;
 import java.io.*;
 import org.json.*;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
@@ -18,7 +19,9 @@ import javax.net.ssl.HttpsURLConnection;
  */
 public class RiskyBot extends TelegramLongPollingBot {
     int gameID = 0;
-    String token = System.getenv("telegramToken");
+    List<Long> ids = new ArrayList<>();
+    // String token = System.getenv("telegramToken");
+    String token = "796705800:AAG4coiT3xqeHmbTfLbPRvMo5K6hEOzzipA";
 
     public RiskyBot() {
         this.gameID = 1;
@@ -27,17 +30,15 @@ public class RiskyBot extends TelegramLongPollingBot {
     public void getUpdates() {
         try {
             URL getUpdates = new URL("https://api.telegram.org/bot" + token + "/getupdates");
-            HttpsURLConnection apiRequest = (HttpsURLConnection)getUpdates.openConnection();
-            BufferedReader in = new BufferedReader(new InputStreamReader(
-                    apiRequest.getInputStream()
-            ));
+            HttpsURLConnection apiRequest = (HttpsURLConnection) getUpdates.openConnection();
+            BufferedReader in = new BufferedReader(new InputStreamReader(apiRequest.getInputStream()));
             String currentLine = "";
             String response = "";
             while ((currentLine = in.readLine()) != null) {
                 // No need to print out anything.
                 response = response + currentLine;
             }
-            //System.out.println(response);
+            // System.out.println(response);
             JSONObject jsonResponse = new JSONObject(response);
             Object results = jsonResponse.get("result");
             System.out.println(results);
@@ -46,13 +47,11 @@ public class RiskyBot extends TelegramLongPollingBot {
         }
     }
 
-    public void reply() {
+    public void reply( ) {
         try {
             URL sendMessage = new URL("https://api.telegram.org/bot" + token + "/sendMessage?");
-            HttpsURLConnection apiRequest = (HttpsURLConnection)sendMessage.openConnection();
-            BufferedReader in = new BufferedReader(new InputStreamReader(
-                    apiRequest.getInputStream()
-            ));
+            HttpsURLConnection apiRequest = (HttpsURLConnection) sendMessage.openConnection();
+            BufferedReader in = new BufferedReader(new InputStreamReader(apiRequest.getInputStream()));
             String currentLine = "";
             String response = "";
             while ((currentLine = in.readLine()) != null) {
@@ -71,10 +70,8 @@ public class RiskyBot extends TelegramLongPollingBot {
     public void getInfo() {
         try {
             URL getInfo = new URL("https://api.telegram.org/bot" + token + "/getMe");
-            HttpsURLConnection apiRequest = (HttpsURLConnection)getInfo.openConnection();
-            BufferedReader in = new BufferedReader(new InputStreamReader(
-                    apiRequest.getInputStream()
-            ));
+            HttpsURLConnection apiRequest = (HttpsURLConnection) getInfo.openConnection();
+            BufferedReader in = new BufferedReader(new InputStreamReader(apiRequest.getInputStream()));
             String currentLine = "";
             String response = "";
             while ((currentLine = in.readLine()) != null) {
@@ -94,7 +91,41 @@ public class RiskyBot extends TelegramLongPollingBot {
         }
     }
 
+    private String checkMessage(long id, String message) {
+        int count = 0;
+        String returnMess = "";
+        if (ids.size() >= 1) {
+            for (int i = 0; i < ids.size(); i++) {
+                System.out.println(id);
+                if (ids.get(i) == id) {
+                    returnMess = "we found you returning player " + message;
+                    break;
+                } else {
+                    count++;
 
+                }
+            }
+            if (count >= ids.size()) {
+                if (message.equals("1234")) {
+                    ids.add(id);
+                    System.out.println("added player " + id);
+                    returnMess = "your in";
+                } else {
+                    returnMess = "sorry if you want to enter a game you need to give me the games id";
+                }
+            }
+        } else {
+            if (message.equals("1234")) {
+                ids.add(id);
+                System.out.println("added player " + id);
+                returnMess = "your in";
+            } else {
+                returnMess = "sorry if you want to enter a game you need to give me the games id";
+            }
+        }
+
+        return returnMess;
+    }
 
     @Override
     public void onUpdateReceived(Update update) {
@@ -104,10 +135,16 @@ public class RiskyBot extends TelegramLongPollingBot {
             // Set variables
             String message_text = update.getMessage().getText();
             long chat_id = update.getMessage().getChatId();
-
+            String responce = "";
+            // String name= update.getMessage().getName();
+            // System.out.println("name is"+name);
+            if (message_text.equals("hello")) {
+                responce = "hello to enter a game you need to type that games id";
+            } else {
+                responce = checkMessage(chat_id, message_text);
+            }
             SendMessage message = new SendMessage() // Create a message object object
-                    .setChatId(chat_id)
-                    .setText(message_text);
+                    .setChatId(chat_id).setText(responce);
             try {
                 execute(message); // Sending our message object to user
             } catch (TelegramApiException e) {
