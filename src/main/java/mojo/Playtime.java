e718398dee0e5b43fd97bf4d58af1dad9e10e1e1
Michael Panagos <michael@technicool.com>
2018-11-15 19:42:27 -0600

    timer now works

--- src/main/java/mojo/Playtime.java
+++ src/main/java/mojo/Playtime.java
@@ -4,20 +4,29 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class Playtime {
-	
-    Timer timer;
+    Timer time = new Timer();
+    int count=0;
 
-    public Playtime(int seconds) {
-        timer = new Timer();
-        timer.schedule(new Tasks(), seconds*1000);
-	}
-
-    class Tasks extends TimerTask {
-        public void run() {
-            System.out.println("Time's up!");
-            timer.cancel(); //Terminate the timer thread
-        }
+    public Playtime() {
+    	
     }
+    
+    public  void startTimer(int seconds) {
+//         count++;
+//         if(count>1){
+//             time.
+//         }
+        
+        time.schedule(new TimerTask(){
+            @Override
+            public void run() {
+                System.out.println("Time's up!");
+                time.cancel(); //Terminate the timer thread
+            }
+        }, seconds*1000);
+    }
+    
+    
 }
 
 //Use to call for 30 seconds: "Playtime play = new Playtime(30);"
