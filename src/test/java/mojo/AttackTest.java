566274bbd4e28b338d47c711204ce61435bfe0fc
Michael Panagos <michael@technicool.com>
2018-11-29 00:49:49 -0600

    added attack test

--- src/test/java/mojo/AttackTest.java
+++ src/test/java/mojo/AttackTest.java
@@ -22,10 +22,13 @@ public class AttackTest{
     List<Player>player=s.getPlayers();
     System.out.println("TEST "+player.get(0).getId());
     player.get(0).addTerritory(new Territory("aaa","a",1,4));
-    player.get(1).addTerritory(new Territory("aaaa","a",2,4));
+    player.get(1).addTerritory(new Territory("aaaa","a",2,5));
+    player.get(1).addTerritory(new Territory("aaaaa","a",2,1));
+    ge.attack(player.get(0).getTerritoryThatCanAttack(0), player.get(1).getTerritoryThatCanAttack(0), 2);
+    System.out.println(ge.attack(player.get(0).getTerritoryThatCanAttack(0), player.get(1).getTerritoryThatCanAttack(0), 3));
 
     System.out.println();
     System.out.println("\n\n\n");
-    assertTrue(" attackTest ",!(ge.attack(player.get(0).getTerritoryThatCanAttack(0), player.get(1).getTerritoryThatCanAttack(0), 1).equals(null)));
+    assertTrue(" attackTest ",!(ge.attack(player.get(0).getTerritoryThatCanAttack(0), player.get(1).getTerritoryThatCanAttack(1), 3).equals(null)));
 }
 }
