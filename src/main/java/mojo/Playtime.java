dc6be0f7ac0c2adf40b828c56b20fd5271f9b564
JarredMcMahan <jtmcmahan@uh.edu>
2018-11-28 22:29:33 -0600

    Edited timer and added timer to RiskyBot

--- src/main/java/mojo/Playtime.java
+++ src/main/java/mojo/Playtime.java
@@ -5,28 +5,18 @@ import java.util.TimerTask;
 
 public class Playtime {
     Timer time = new Timer();
-    int count=0;
-
-    public Playtime() {
-    	
-    }
-    
+    boolean done;
     public  void startTimer(int seconds) {
-//         count++;
-//         if(count>1){
-//             time.
-//         }
-        
+        done = false;
         time.schedule(new TimerTask(){
             @Override
             public void run() {
                 System.out.println("Time's up!");
+                done = true;
                 time.cancel(); //Terminate the timer thread
             }
         }, seconds*1000);
     }
-    
-    
 }
 
 //Use to call for 30 seconds: "Playtime play = new Playtime(30);"
