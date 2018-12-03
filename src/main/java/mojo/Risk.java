a8d262ac85cbc99fb2ed80dc441de170e34d1e10
Oscar Tovar <oscar.alberto.tovar@gmail.com>
2018-12-03 03:34:52 -0600

    Game now has a replay mode.

--- src/main/java/mojo/Risk.java
+++ src/main/java/mojo/Risk.java
@@ -9,6 +9,8 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import mojo.WebConnection;
 
+import java.util.Scanner;
+
 /**
  * @author TeamMojo
  *
@@ -20,13 +22,19 @@ public class Risk {
 	 */
 	public static void main(String[] args) {
 	    // Check to see if the game is in replay mode.
-		if (args.length > 1 && args[1].equals("replay")) {
+        System.out.println("What would you like to do?");
+        System.out.println("1. Replay");
+        System.out.println("2. New Game");
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+
+		if (choice == 1) {
             // Do replay here.
             System.out.println("Initializing replay!");
             WebConnection webConnection = new WebConnection();
             System.out.println(webConnection.replay());
         }
-        else  {
+        else if (choice == 2) {
             ApiContextInitializer.init();
 
             // Instantiate Telegram Bots API
@@ -38,6 +46,9 @@ public class Risk {
             } catch (TelegramApiException e) {
                 e.printStackTrace();
             }
+        }
+        else {
+            System.out.println("Invalid option. Run the program again.");
         }
 	}
 
